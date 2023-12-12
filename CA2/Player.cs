f0332d80f0ffb9300ff2e9802b70c62a380f49e8
Jake Smith <S00234729@atu.ie>
2023-12-12 20:26:01 +0000

    Q1(d) completed

---
+++ CA2/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public class Player
+    {
+        //set up vars
+        string Name;
+        string ResultRecord;
+
+        //French players
+        Player p1 = new Player() { Name = "Marie", ResultRecord = "WWDDL" };
+        Player p2 = new Player() { Name = "Claude", ResultRecord = "DDDLW" };
+        Player p3 = new Player() { Name = "Antoine", ResultRecord = "LWDLW" };
+
+        //Italian players
+        Player p4 = new Player() { Name = "Marco", ResultRecord = "WWDLL" };
+        Player p5 = new Player() { Name = "Giovanni", ResultRecord = "LLLLD" };
+        Player p6 = new Player() { Name = "Valentina", ResultRecord = "DLWWW" };
+
+        //Spanish players
+        Player p7 = new Player() { Name = "Maria", ResultRecord = "WWWWW" };
+        Player p8 = new Player() { Name = "Jose", ResultRecord = "LLLLL" };
+        Player p9 = new Player() { Name = "Pablo", ResultRecord = "DDDDD" };
+
+    }
+}
